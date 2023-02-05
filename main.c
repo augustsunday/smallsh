bbc30ff6c6a1e8b1b7869755a7a387422af3b595
augustsunday <all.our.august.sundays@gmail.com>
2023-02-05 11:51:52 -0800

    Implemented WORD_LIMIT error checking for number of args in a line. Jumps back to head of loop if the line is empty. Displays error and jumps back to head of loop if line has too many args

--- main.c
+++ main.c
@@ -15,18 +15,47 @@
 #define dprintf(...) ((void)0)
 #endif
 
+const int WORD_LIMIT = 512;
 
+int split_input(char **words, char *line_ptr) {
+    char* restrict word;
+    char* restrict delim = getenv("IFS");
 
+    if (!delim) delim = " \t\n";
+    word = strtok(line_ptr, delim);
+    int i = 0;
+    while ((word != NULL) & (i < WORD_LIMIT)) {
+      words[i] = strdup(word);
+      ++i;
+      word = strtok(NULL, delim);
+    };
+
+    if (i == WORD_LIMIT && word != NULL) {
+      return -1;
+    }
+
+    return i;
+}
+
+int execute(char  *restrict *restrict args) {
+  if (*args == NULL) {
+    dprintf("No command word detected. Return to prompt.\n");
+    return 0;
+  }
+  dprintf("Command word: %s\n",*args);
+  return 1;
+
+}
 
 int main(void) {
-
   char* line_ptr = NULL;
   size_t line_len = 0;
   ssize_t read;
-  char** restrict words = calloc(513, sizeof (*words));
-  char* restrict word = malloc(sizeof word);
-  char* restrict delim = NULL;
-  char* const restrict env_ifs = "IFS";
+  char** words = NULL;
+  char* prompt = NULL;
+  int words_read = 0;
+
+  words = malloc(sizeof *words * WORD_LIMIT); 
 
   for (;;) {
     /* TODO: Realloc everything */
@@ -35,36 +64,31 @@ int main(void) {
     /* Manage background processes */
 
     /* Prompt */
-    char *prompt = getenv("PS1");
-    dprintf("PS1 Prompt: %s",prompt);
+    prompt = getenv("PS1");
+    dprintf("PS1 Prompt: %s\n",prompt);
     if (prompt == 0) {
       perror("Error: prompt");
-      goto cleanup;
     }
-    printf("%s", prompt);
+    fprintf(stderr, "%s", prompt);
 
     /* Read a line of input */
     read = getline(&line_ptr, &line_len, stdin);
     dprintf("Read %ld chars from line\n", read);
     if (read == -1) {
       perror("error: getline");
-      goto cleanup;
+      exit(EXIT_FAILURE);     
     } else if (read == 0) {
       continue;
     }
     dprintf("Line input: %s\n", line_ptr);
 
     /* Split input */
-    delim = getenv(env_ifs);
-    if (!delim) delim = " \t\n";
-    dprintf("Current IFS delimiter: %s", delim);
-    word = strtok(line_ptr, delim);
-    while (word != NULL) {
-      *words = strdup(word);
-      /* TODO null handling */
-      dprintf("Copied %s to args",*words);
-      words += 1;
-      word = strtok(NULL, delim);
+    words_read = split_input(words, line_ptr);
+    if (words_read == -1) {
+      fprintf(stderr, "smallsh: Exceeded max arguments");
+      continue;
+    } else if (words_read == 0) {
+      continue;
     }
 
     /* Expand */
@@ -72,6 +96,8 @@ int main(void) {
     /* Parse */
 
     /* Execute */
+    execute(words);
+
 
    
     
